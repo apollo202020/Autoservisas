45543772b9d350a2f4dfb36679e9fd3adb0a854c
Edvinas Sudzius <edvinass20@gmail.com>
2023-04-26 22:58:24 +0200

    +Padaryti, kad Paslaugų sąraše būtų matomi paslaugos pavadinimų ir kainų stulpeliai

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -11,13 +11,16 @@ class OrderLineInline(admin.TabularInline):
 
 class VehicleAdmin(admin.ModelAdmin):
     list_display = ["vehicle_model", "owner_name", "plate", "vin"]
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ["name", "price"]
 class OrderAdmin(admin.ModelAdmin):
     list_display = ["vehicle", "date"]
     inlines = [OrderLineInline]
 
 # Register your models here.
 admin.site.register(VehicleModel)
-admin.site.register(Service)
+admin.site.register(Service, ServiceAdmin)
 admin.site.register(Vehicle, VehicleAdmin)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderLine)
