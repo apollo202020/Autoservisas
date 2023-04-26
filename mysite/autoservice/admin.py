1200a620b7d92fbd11d6989cc884deb3fadae92f
Edvinas Sudzius <edvinass20@gmail.com>
2023-04-26 22:55:51 +0200

    +Padaryti, kad automobilių sąraše būtų matomi kliento, automobilio, valstybinio numerio ir VIN numerio stulpeliai

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -8,6 +8,9 @@ from .models import (VehicleModel,
 class OrderLineInline(admin.TabularInline):
     model = OrderLine
     extra = 0
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ["vehicle_model", "owner_name", "plate", "vin"]
 class OrderAdmin(admin.ModelAdmin):
     list_display = ["vehicle", "date"]
     inlines = [OrderLineInline]
@@ -15,6 +18,6 @@ class OrderAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(VehicleModel)
 admin.site.register(Service)
-admin.site.register(Vehicle)
+admin.site.register(Vehicle, VehicleAdmin)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderLine)
